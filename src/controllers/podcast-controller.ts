3e39e82890d2cfc31cb62de97b1502e390889b77
Milena Pessoa <milenapessoa.am@gmail.com>
2026-09-19 12:27:32 -0300

    feat: adiciona rotas de listagem e filtro de podcasts

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -1,9 +1,17 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { serviceListEpisode } from "../services/list-episodes-service";
+import { serviceFilterEpisodes } from "../services/filter-episodes-service";
 
 export const getListEpisodes = async (req: IncomingMessage, res:ServerResponse) => {
     const content = await serviceListEpisode();
 
     res.writeHead(200, {"Content-Type":"application/json"});
     res.end(JSON.stringify(content));
+}
+
+export const getFilterEpisodes = async (req: IncomingMessage, res:ServerResponse) => {
+    const content = await serviceFilterEpisodes();
+
+    res.writeHead(200, {"Content-Type":"application/json"});
+    res.end(JSON.stringify(content)); 
 }
